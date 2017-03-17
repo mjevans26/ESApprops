1d26bdd93dfb36c136f8dcb9a22abd53f1e1fa6d
mjevans26 <evans.michael26@gmail.com>
2017-03-17 11:13:33 -0400

    Text edits, added then deleted other new format files

--- inst/esa-appropriations/ui.R
+++ inst/esa-appropriations/ui.R
@@ -14,27 +14,40 @@ sidebar <-  dashboardSidebar(disable = TRUE)
 
 body <- dashboardBody(
   tags$head(
+    tags$link(rel = "styleshet", type = "text/css", href = "bootstrap.css"),
     tags$link(rel = "stylesheet", type = "text/css", href = "custom_styles.css")),
 
-  navbarPage(div(column(4,tags$a(href = "http://www.defenders.org", tags$img(src = "01_DOW_LOGO_COLOR_300-01.png", height = "80px"))), column(8, h4("Defenders of Widlife", br(), "Center for Conservation Innovation"))),
-   tabPanel(h4("Overview"),id = "summary", fluidPage(
-    fluidRow(
-     column(2,br()),
-     column(8, h2("The Importance of Properly Funding the ESA"),
-      h4("Since 1973, the Endangered Species Act (ESA) has been the most important U.S. law protecting
+  navbarPage(collapsible = TRUE,
+             position = 'static-top',
+             #div(
+             # column(4,
+                 #   tags$a(href = "http://www.defenders.org",
+                 #          tags$img(src = "01_DOW_LOGO_COLOR_300-01.png", height = "80px"))),
+             # column(8, h4("Defenders of Widlife", br(), "Center for Conservation Innovation"))),
+             title = a(href = "http://www.defenders.org", h4("Defenders of Wildlife, CCI")),
+   tabPanel(h4("Overview"),id = "summary",
+    fluidPage(
+     fluidRow(br()),
+     fluidRow(
+      column(2,br()),
+      column(8, h2("The Importance of Properly Funding the ESA"),
+       h4("Since 1973, the Endangered Species Act (ESA) has been the most important U.S. law protecting
          plants and animals at risk of extinction. The ESA conserves species by regulating activities that may harm species and by
          facilitating species recovery. The U.S. Fish and Wildlife Service (FWS) is responsible for conserving the majority of
          listed species under the ESA, a function that has been impeded by inadequate funding from Congress."),
-      br(),
-      imageOutput("foxes"),
-      h6("Photo courtesy of National Park Service"),
-      br(),
-      h4("Several fox species", tags$em(" (Urocyon littoralis) "), "of the Channel Islands were recovered and delisted in 2016.  Success stories such as these highlight
+       br(),
+       tags$figure(class = "figure",
+                   tags$img(src = "Island_Fox.jpg",
+                            class = "img-responsive",
+                            alt = "Island fox"),
+                   tags$figcaption(class = "figure-caption", "Photo courtesy of National Park Service")),
+       br(),
+       h4("Several fox species", tags$em(" (Urocyon littoralis) "), "of the Channel Islands were recovered and delisted in 2016.  Success stories such as these highlight
          the importance of properly funding the ESA.")
-     ),
-     column(2,br())
-     ),br(),
-    fluidRow(column(2),
+      ),
+      column(2,br())
+      ),
+     fluidRow(column(2, br()),
              column(4, div(HTML('<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
                                 <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
                                 <br />
@@ -43,45 +56,58 @@ body <- dashboardBody(
                                 is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
                                 <br />'),
                            style = "text-align: center")),
-             column(1),
-             column(2, div(HTML('<br /> Email questions or comments to <a href = "mailto:esa@defender.org"> esa@defenders.org </a>'), style = "text-align: center"))
-             )
-    )),
+             column(1, br()),
+             column(2,
+                    div(HTML('<br /> Email questions or comments to <a href = "mailto:esa@defender.org"> esa@defenders.org </a>'),
+                        style = "text-align: center"))
+      )
+     )
+    ),
 
-   tabPanel(h4("Funding Needs"), id = "panel2", fluidPage(
-    fluidRow(column(3, br()), column(9, h4("Species Recovery Funding and Status"))),
-    fluidRow(
-     column(4, plotlyOutput("pie"), height = 600),
-     column(4, plotlyOutput("bar"), height = 600),
-     column(4,
-      h2("Species Recovery Has Been",br(),"Chronically Underfunded"),
-      h4("Congress has provided only a small fraction of the funds needed for recovery.
+   tabPanel(h4("Funding Needs"), id = "panel2",
+    fluidPage(
+     fluidRow(br()),
+     fluidRow(column(3, br()),
+              column(9, h4("Species Recovery Funding and Status"))
+              ),
+     fluidRow(
+      column(4, plotlyOutput("pie"), height = 600),
+      column(4, plotlyOutput("bar"), height = 600),
+      column(4,
+       h2("Species Recovery Has Been",br(),"Chronically Underfunded"),
+       h4("Congress has provided only a small fraction of the funds needed for recovery.
           Data from Gerber (2016) shown here illustrate this shortchanging.
          From 1980 to 2014, the vast majority of listed species with recovery plans were underfunded - receiving
-         less than 90% of the amount needed for recovery.  Because funding is correlated with
-         improvement in species status, only 15% of species improved in status from 1989 - 2011."),
-      br(),
-      h6("Source: ", a(href = "http://www.pnas.org/content/113/13/3563.full?tab=ds", "Gerber, LR (2016). PNAS 113(13): 3563-3566"))
+         less than 90% of the amount needed for recovery."),
+       br(),
+       h6("Source: ", a(href = "http://www.pnas.org/content/113/13/3563.full?tab=ds", "Gerber, LR (2016). PNAS 113(13): 3563-3566"))
       )
+     )
     )
-   )),
+   ),
 
-   tabPanel(h4("Funding Trends"), id = "panel3", fluidPage(
+   tabPanel(h4("Funding Trends"), id = "panel3",
+    fluidPage(
+     fluidRow(br()),
      fluidRow(
        column(8, plotlyOutput("appropriation"), height = 600),
        column(4,
               h2("Funding Has Not Kept Pace",br(),"with Species Listings"),
-              h4("Not only has Congress underfunded recovery for decades, but the average dollar per species has decreased since 2010.
+              h4("Not only has Congress underfunded recovery for decades, but the amount of funding per species has decreased since 2010.
                  This drop has occurred across all endangered species programs at FWS.
-                 This decline is the result of the ESA budget consistently decreasing, combined with the continued increase in the number of listed species.",
+                 This decline is the result of the ESA budget decreasing since 2010, combined with the continued increase in the number of listed species.
+                 Prior to this decline, the per species budget had been stagnant for almost a decade.",
                  br(),br(), "Use the dropdown button to see total annual appropriations."),
               br(),
               h6("Source: ", a(href = "https://www.fws.gov/budget/", "FWS Budget Justification"))
        )
      )
-   )),
+    )
+   ),
 
-   tabPanel(h4("Spending Trends"), id = "panel4", fluidPage(
+   tabPanel(h4("Spending Trends"), id = "panel4",
+    fluidPage(
+     fluidRow(br()),
      fluidRow(
        column(8, plotlyOutput("spending"), height = 600),
        column(4,
@@ -97,9 +123,11 @@ body <- dashboardBody(
               h6("Source: ", a(href = "https://www.fws.gov/Endangered/esa-library/index.html", "FWS Expenditure Reports"))
        )
      )
-   )),
+    )
+   ),
 
    tabPanel(h4("State Spending"), id = "panel5", fluidPage(
+     fluidRow(br(),br(),br()),
      fluidRow(
        column(8, plotlyOutput("years"), height = 600),
        column(4,
